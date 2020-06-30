b478ca2d87ad911bc1a4a8cd88a0f9d534daf937
Yerin Jung <yell_10@naver.com>
2020-06-30 22:52:00 +0900

    저장한 메모 불러오기 기능, 메모 내용 저장하기 기능, 저장된 메모 삭제하기 기능 생성하기

--- app/src/main/java/com/professionalandroid/apps/mymemoversimple/MainActivity.java
+++ app/src/main/java/com/professionalandroid/apps/mymemoversimple/MainActivity.java
@@ -2,24 +2,72 @@ package com.professionalandroid.apps.mymemoversimple;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+    public class MainActivity extends AppCompatActivity {
 
 
-public class MainActivity extends AppCompatActivity {
+        EditText mMemoEdit = null;
+        TextFile mTextFileManager = new TextFile(this);
+        // 현재시간을 current에 저장
+        long current = System.currentTimeMillis();
 
+        // 현재시간을 date 변수에 저장
+        Date date = new Date(current);
 
-    EditText MemoEdit = null;
-    TextFile TextFile = new TextFile(this);
+        // 메모장 앱에 들어간 시간 => 출력방식: yyyy.MM.dd HH:mm:ss
+        SimpleDateFormat SimpleDateFormat_Now = new SimpleDateFormat("메모장 들어온 시간 : " + "yyyy.MM.dd HH:mm:ss");
 
+        // 변수에 값을 저장
+        String formatDate = SimpleDateFormat_Now.format(date);
 
+        TextView date_current;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            date_current = (TextView) findViewById(R.id.date_current);
+            date_current.setText(formatDate);
 
-        MemoEdit = (EditText) findViewById(R.id.memo_edit);
+            mMemoEdit = (EditText) findViewById(R.id.memo_edit);
+        }
 
+        public void onClick(View v) {
+            switch (v.getId()) {
+                // 저장한 메모 파일로 불러오기
+                case R.id.buttonload: {
+                    String memoData = mTextFileManager.load();
+                    mMemoEdit.setText(memoData);
+
+                    Toast.makeText(this, "불러오기 실행 완료", Toast.LENGTH_LONG).show();
+                    break;
+                }
+
+                // 입력한 메모 내용을 파일로 저장하기
+                case R.id.buttonsave: {
+                    String memoData = mMemoEdit.getText().toString();
+                    mTextFileManager.save(memoData);
+                    mMemoEdit.setText("");
+
+                    Toast.makeText(this, "저장 실행 완료", Toast.LENGTH_LONG).show();
+                    break;
+                }
+
+                // 저장된 메모 파일 삭제하기
+                case R.id.buttondelete: {
+                    mTextFileManager.delete();
+                    mMemoEdit.setText("");
+
+                    Toast.makeText(this, "삭제 실행 완료", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
 }
